7f95d52f9a0df981fa021b336c6cac91edeedec0
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-22 13:12:49 +0530

    feat: implement canvas drawing functionality in Room component

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 const Room = () => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    // Example: Draw a red rectangle
+    ctx.fillStyle = 'red';
+    ctx.fillRect(20, 20, 150, 100);
+    }, [])
+    
   return (
-    <div>
-      
+    <div className='flex justify-center'>
+      <canvas id="myCanvas" className='bg-red-100 w-[1200px] h-[800px] rounded-lg'ref={canvasRef}></canvas>
     </div>
   )
 }
